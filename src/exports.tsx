81070247ad7bbdab10090b1eb25663be7c79cd8f
openrpc-bastion <openrpc-bastion@protonmail.com>
2020-05-06 03:52:11 +0000

    github pages release

--- src/exports.tsx
+++
@@ -1,2 +0,0 @@
-import {default as Inspector} from "./containers/Inspector";
-export default Inspector;
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import App from "./containers/App";
-import "./App.css";
-import "./splitpane.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
